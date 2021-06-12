936909a6243d89e75f9bdbf44e3ceeef16347548
qinya <qinya@cmiot.chinamobile.com>
2021-06-12 11:37:14 +0800

    init 实现deploy+svc一键启动operator

---
+++ controllers/appservice_controller.go
@@ -0,0 +1,246 @@
+/*
+Copyright 2021 qinya0.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	v1 "k8s.io/api/admission/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"reflect"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	appv1 "github.com/qinya0/k8s-operator-demo/api/v1"
+)
+
+// AppServiceReconciler reconciles a AppService object
+type AppServiceReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=app.qy.com,resources=appservices,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=app.qy.com,resources=appservices/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=app.qy.com,resources=appservices/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the AppService object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
+
+func (r *AppServiceReconciler) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
+	reqLogger := log.FromContext(ctx).WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	reqLogger.Info("Reconciling AppService")
+
+	// Fetch the AppService instance
+	instance := &appv1.AppService{}
+	err := r.Client.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			reqLogger.Info("not found")
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	if instance.DeletionTimestamp != nil {
+		reqLogger.Info(" instance DeletionTimestamp")
+		return reconcile.Result{}, err
+	}
+
+	// 如果不存在，则创建关联资源
+	// 如果存在，判断是否需要更新
+	//   如果需要更新，则直接更新
+	//   如果不需要更新，则正常返回
+
+	deploy := &appsv1.Deployment{}
+	if err := r.Client.Get(context.TODO(), request.NamespacedName, deploy); err != nil && errors.IsNotFound(err) {
+		// 创建关联资源
+		// 1. 创建 Deploy
+		deploy := NewDeploy(instance)
+		if err := r.Client.Create(context.TODO(), deploy); err != nil {
+			return reconcile.Result{}, err
+		}
+		reqLogger.Info("create deploy")
+		// 2. 创建 Service
+		service := NewService(instance, nil)
+		if err := r.Client.Create(context.TODO(), service); err != nil {
+			return reconcile.Result{}, err
+		}
+		reqLogger.Info("create svc")
+		// 3. 关联 Annotations
+		data, _ := json.Marshal(instance.Spec)
+		if instance.Annotations != nil {
+			instance.Annotations["spec"] = string(data)
+		} else {
+			instance.Annotations = map[string]string{"spec": string(data)}
+		}
+
+		if err := r.Client.Update(context.TODO(), instance); err != nil {
+			return reconcile.Result{}, nil
+		}
+		reqLogger.Info("update instance")
+		return reconcile.Result{}, nil
+	}
+
+	oldspec := appv1.AppServiceSpec{}
+	if err := json.Unmarshal([]byte(instance.Annotations["spec"]), &oldspec); err != nil {
+		return reconcile.Result{}, err
+	}
+
+	if !reflect.DeepEqual(instance.Spec, oldspec) {
+		// 更新关联资源
+		newDeploy := NewDeploy(instance)
+		oldDeploy := &appsv1.Deployment{}
+		if err := r.Client.Get(context.TODO(), request.NamespacedName, oldDeploy); err != nil {
+			return reconcile.Result{}, err
+		}
+		oldDeploy.Spec = newDeploy.Spec
+		if err := r.Client.Update(context.TODO(), oldDeploy); err != nil {
+			return reconcile.Result{}, err
+		}
+		reqLogger.Info("update deploy")
+
+		oldService := &corev1.Service{}
+		if err := r.Client.Get(context.TODO(), request.NamespacedName, oldService); err != nil {
+			return reconcile.Result{}, err
+		}
+		newService := NewService(instance, oldService)
+		oldService.Spec = newService.Spec
+		if err := r.Client.Update(context.TODO(), oldService); err != nil {
+			return reconcile.Result{}, err
+		}
+		reqLogger.Info("update svc")
+
+		return reconcile.Result{}, nil
+	}
+
+	return reconcile.Result{}, nil
+
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *AppServiceReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&appv1.AppService{}).
+		Complete(r)
+}
+
+func NewDeploy(app *appv1.AppService) *appsv1.Deployment {
+	labels := map[string]string{"app": app.Name}
+	selector := &metav1.LabelSelector{MatchLabels: labels}
+	return &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apps/v1",
+			Kind:       "Deployment",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      app.Name,
+			Namespace: app.Namespace,
+
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(app, schema.GroupVersionKind{
+					Group:   v1.SchemeGroupVersion.Group,
+					Version: v1.SchemeGroupVersion.Version,
+					Kind:    "AppService",
+				}),
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &app.Spec.Size,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: newContainers(app),
+				},
+			},
+			Selector: selector,
+		},
+	}
+}
+
+func newContainers(app *appv1.AppService) []corev1.Container {
+	containerPorts := []corev1.ContainerPort{}
+	for _, svcPort := range app.Spec.Ports {
+		cport := corev1.ContainerPort{}
+		cport.ContainerPort = svcPort.TargetPort.IntVal
+		containerPorts = append(containerPorts, cport)
+	}
+	return []corev1.Container{
+		{
+			Name:            app.Name,
+			Image:           app.Spec.Image,
+			Resources:       app.Spec.Resources,
+			Ports:           containerPorts,
+			ImagePullPolicy: corev1.PullIfNotPresent,
+			Env:             app.Spec.Envs,
+		},
+	}
+}
+
+func NewService(app *appv1.AppService, oldSvc *corev1.Service) *corev1.Service {
+	newSvc := &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      app.Name,
+			Namespace: app.Namespace,
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(app, schema.GroupVersionKind{
+					Group:   v1.SchemeGroupVersion.Group,
+					Version: v1.SchemeGroupVersion.Version,
+					Kind:    "AppService",
+				}),
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Type:  corev1.ServiceTypeNodePort,
+			Ports: app.Spec.Ports,
+			Selector: map[string]string{
+				"app": app.Name,
+			},
+		},
+	}
+
+	if oldSvc != nil {
+		// 增加旧的port里面的已有项
+		newSvc.Spec.ClusterIP = oldSvc.Spec.ClusterIP
+	}
+	return newSvc
+}
